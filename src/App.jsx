9716ab7025623a30d0762b3de5af7f3cd0ebfeec
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-23 09:06:54 -0300

    ejercicio 3 terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const incrementar = ()=>{
+    setCount(count + 1)
+  }
+
   return (
     <>
     <p>{count}</p>
+    <button onClick={incrementar} >Incrementar</button>
     </>
   )
 }
